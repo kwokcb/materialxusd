edbb220e1930bbcfc61cc0dae1fad7667e773917
Bernard Kwok <kwokcb@gmail.com>
2025-02-20 15:12:56 -0500

    cleanup internal utils.

--- utilities_internal/usd_setup.sh
+++
@@ -1,4 +0,0 @@
-# Set up environment for runnin USD
-# Set up environment for running USD
-export PYTHONPATH=/Users/bernardkwok/work/USD_build/lib/python:$PYTHONPATH
-export PATH=/Users/bernardkwok/work/USD_build/bin:$PATH

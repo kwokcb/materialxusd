e78304060be3b7e90baa1cd7d2f756d2cc11067f
Bernard Kwok <kwokcb@gmail.com>
2025-03-14 16:00:32 -0400

    Doc update.

--- tests/create_summary.sh
+++ tests/create_summary.sh
@@ -1,2 +1,2 @@
-python ../utilities/create_image_path.py ./resources/Materials --scroll -r "https://kwokcb.github.io/materialxusd/tests"
-python ../utilities/create_image_path.py -hd "PhysicallYBased Materials USD Gallery" ./physically_based -o usd_physicallY_based_gallery.html --scroll -r "https://kwokcb.github.io/materialxusd/tests"
+python ../utilities/create_image_path.py ./resources/Materials  -r "https://kwokcb.github.io/materialxusd/tests"
+python ../utilities/create_image_path.py -hd "PhysicallyBased Materials USD Gallery" ./physically_based -o usd_physicallY_based_gallery.html  -r "https://kwokcb.github.io/materialxusd/tests"
